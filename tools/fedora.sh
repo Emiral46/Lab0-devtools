0b470feee9f025ba75430ddf3c1632a42951e03f
Emiral46 <156061490+Emiral46@users.noreply.github.com>
2025-10-31 13:03:37 +0000

    Add files via upload

---
+++ tools/fedora.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# Author : Emir Alemdar
+# Year   : 2025
+
+echo () {
+    printf "\n%b\n" "[iac] $1"
+}
+
+echo "Updating dnf packages... you may have to enter your password here"
+sudo dnf upgrade
+
+echo "Installing dependencies.."
+sudo dnf install git \
+    perl \
+    python3 \
+    python3-pip \
+    gperf \
+    autoconf \
+    bc \
+    bison \
+    gcc \
+    clang \
+    make
+
+sudo dnf group install "development-tools"
+
+sudo dnf install \
+    flex \
+    ca-certificates \
+    ccache \
+    gperftools-devel \
+    numactl \
+    perl-doc \
+    libfl2 \
+    libfl-devel \
+    zlib-ng \
+    zlib-ng-devel \
+    qemu qemu-user \
+    gtkwave \
+    jq
+
+# Install Verilator
+echo "Installing Verilator"
+sudo dnf install verilator
+verilator --version
+
+echo "Installing riscv-gnu-toolchain... this may require your password..."
+# shellcheck disable=SC1091
+tools_download_link="https://github.com/Emiral46/Lab0-devtools/releases/download/v1.0.0/riscv-gnu-toolchain-2022-09-21-Linux-x86_64.tar.gz"
+
+cd /tmp
+rm -rf riscv-gnu-toolchain.tar.gz
+curl --output riscv-gnu-toolchain.tar.gz -L "${tools_download_link}"
+sudo rm -rf /opt/riscv
+sudo tar -xzf riscv-gnu-toolchain.tar.gz --directory /opt
+export PATH="/opt/riscv/bin:$PATH"
+if ! grep "/opt/riscv/bin" ~/.bashrc > /dev/null; then
+    # shellcheck disable=SC2016
+    printf '\n%s' 'export PATH="/opt/riscv/bin:$PATH"' >> ~/.bashrc
+fi
+rm -rf riscv-gnu-toolchain.tar.gz
